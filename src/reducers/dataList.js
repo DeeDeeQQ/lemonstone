019436a35117ef0031b378160a0626731f6de6d9
Ihor Ruzhenko <deedeeqqs@gmail.com>
2018-08-19 22:39:14 +0300

    merge and filter data from api

--- src/reducers/dataList.js
+++ src/reducers/dataList.js
@@ -1,6 +1,18 @@
 export default function dataList(state = [], action) {
-    if (action.type === "GET_LIST") {
-        return [...action.payload];
-    }
-    return state;
+  let tmp = {};
+
+  function filter(arr) {
+    return arr.filter(function(a) {
+      return a.objectID in tmp ? 0 : (tmp[a.objectID] = 1);
+    });
+  }
+
+  if (action.type === "GET_LIST") {
+    let result = state.concat(action.payload);
+
+    let newData = filter(result);
+
+    return [...newData];
+  }
+  return state;
 }
